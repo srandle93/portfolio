09dc2a89039ff6ec4a4b8f05e8239be6c9d088a7
srandle93 <randle.symone.l@gmail.com>
2018-11-03 21:05:16 -0500

    Changed portfolio.html to index.html

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 
 // routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/portfolio.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/contact", (req, res) => {
